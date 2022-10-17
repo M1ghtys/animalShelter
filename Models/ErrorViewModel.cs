bc74e50041116f176c23c819fa52a8b4bca42a0e
Jakub Sychra <kuba@sychra.com>
2022-10-17 23:07:01 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
